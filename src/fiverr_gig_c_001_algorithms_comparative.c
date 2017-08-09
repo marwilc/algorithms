c5edc1b89e6561779eec6d245394bd43a5c3c271
Marwil Campos <marwilcampos@gmail.com>
2017-08-08 21:30:19 -0400

    git init

---
+++ src/fiverr_gig_c_001_algorithms_comparative.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : fiverr_gig_c_001_algorithms_comparative.c
+ Author      : Marwil Campos
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
